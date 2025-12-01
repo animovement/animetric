4c9f6068bf9e356f4681061576a47104391a4d2a
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2025-12-01 13:36:47 +0100

    air formatting

--- tests/testthat/test-summarise_angle.R
+++ tests/testthat/test-summarise_angle.R
@@ -13,32 +13,32 @@
 # - Odd and even length vectors
 
 test_that("mean_angle returns values in [0, 2*pi)", {
-  result <- mean_angle(c(0, pi/2, pi, 3*pi/2))
+  result <- mean_angle(c(0, pi / 2, pi, 3 * pi / 2))
   expect_gte(result, 0)
-  expect_lt(result, 2*pi)
+  expect_lt(result, 2 * pi)
 })
 
 test_that("mean_angle handles angles outside [0, 2*pi)", {
-  result1 <- mean_angle(c(-pi/2, pi/2))
-  result2 <- mean_angle(c(3*pi/2, pi/2))
+  result1 <- mean_angle(c(-pi / 2, pi / 2))
+  result2 <- mean_angle(c(3 * pi / 2, pi / 2))
   expect_equal(result1, result2)
 
-  result3 <- mean_angle(c(0, 4*pi))
+  result3 <- mean_angle(c(0, 4 * pi))
   result4 <- mean_angle(c(0, 0))
   expect_equal(result3, result4)
 })
 
 test_that("mean_angle handles single angle", {
-  angle <- pi/4
+  angle <- pi / 4
   result <- mean_angle(angle)
   expect_equal(result, angle)
 })
 
 test_that("mean_angle is consistent", {
-  angles <- seq(0, 2*pi, length.out = 7)[-7]
+  angles <- seq(0, 2 * pi, length.out = 7)[-7]
   result <- mean_angle(angles)
   expect_gte(result, 0)
-  expect_lt(result, 2*pi)
+  expect_lt(result, 2 * pi)
 })
 
 test_that("mean_angle handles clustered angles", {
@@ -49,38 +49,38 @@ test_that("mean_angle handles clustered angles", {
 })
 
 test_that("median_angle returns values in [0, 2*pi)", {
-  result <- median_angle(c(0, pi/2, pi, 3*pi/2))
+  result <- median_angle(c(0, pi / 2, pi, 3 * pi / 2))
   expect_gte(result, 0)
-  expect_lt(result, 2*pi)
+  expect_lt(result, 2 * pi)
 })
 
 test_that("median_angle handles angles outside [0, 2*pi)", {
-  result1 <- median_angle(c(-pi/2, pi/2))
-  result2 <- median_angle(c(3*pi/2, pi/2))
+  result1 <- median_angle(c(-pi / 2, pi / 2))
+  result2 <- median_angle(c(3 * pi / 2, pi / 2))
   expect_equal(result1, result2)
 
-  result3 <- median_angle(c(0, 4*pi))
+  result3 <- median_angle(c(0, 4 * pi))
   result4 <- median_angle(c(0, 0))
   expect_equal(result3, result4)
 })
 
 test_that("median_angle handles single angle", {
-  angle <- pi/4
+  angle <- pi / 4
   result <- median_angle(angle)
   expect_equal(result, angle)
 })
 
 test_that("median_angle handles odd and even length vectors", {
-  odd_vec <- c(0, pi/4, pi/2)
-  even_vec <- c(0, pi/4, pi/2, 3*pi/4)
+  odd_vec <- c(0, pi / 4, pi / 2)
+  even_vec <- c(0, pi / 4, pi / 2, 3 * pi / 4)
 
   result_odd <- median_angle(odd_vec)
   result_even <- median_angle(even_vec)
 
   expect_gte(result_odd, 0)
-  expect_lt(result_odd, 2*pi)
+  expect_lt(result_odd, 2 * pi)
   expect_gte(result_even, 0)
-  expect_lt(result_even, 2*pi)
+  expect_lt(result_even, 2 * pi)
 })
 
 test_that("median_angle handles clustered angles", {
